6ec4c2cb2841833eef86242f66ecbb59bccfddae
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-09-18 22:12:51 +0530

    basic program

---
+++ Addition.java
@@ -0,0 +1,11 @@
+public class Addition{
+    public static void main(String args[]){
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        // System.out.println(f"sum is {sum}"); there no concept of f-String in java like python does
+        System.out.println("sum is : " + sum);
+        
+        
+    }
+}
