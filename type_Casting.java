3fa115f360fbc0463065cf5814cab16a767fe383
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-09-21 14:19:34 +0530

    narrowing conversion

---
+++ type_Casting.java
@@ -0,0 +1,23 @@
+public class type_Casting{
+    public static void main(String args[]){
+        float f = 35.78f;
+        int a = (int)f;
+        System.out.println(a);
+
+        char ch1 = 'a';
+        int i = ch1;
+        System.out.println(i);
+
+        char ch2 = 'b';
+        int j = ch2;
+        System.out.println(j);
+
+        char ch3 = 'A';
+        int k = ch3;
+        System.out.println(k);
+
+        char ch4 = '@';
+        int l = ch4;
+        System.out.println(l); 
+    }
+}
