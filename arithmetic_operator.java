3b30fa117b1a12103916adb4c48811d12ecf0c92
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-09-22 20:12:55 +0530

    operators

---
+++ arithmetic_operator.java
@@ -0,0 +1,16 @@
+public class arithmetic_operator{
+    public static void main(String args[]){
+        int a = 10;
+        int b = 5;
+        int add = a + b;
+        System.out.println(add);
+        int subtract = a - b;
+        System.out.println(subtract);
+        int multiplication = a * b;
+        System.out.println(multiplication);
+        int division = a / b;
+        System.out.println(division);
+        int modulo = a % b;
+        System.out.println(modulo);
+    }
+}
