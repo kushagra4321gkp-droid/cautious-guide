afb05d14d104faa895ab7b6883862fe0787c2289
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-09-23 20:04:50 +0530

    just revision

--- arithmetic_operator.java
+++ arithmetic_operator.java
@@ -14,3 +14,37 @@ public class arithmetic_operator{
         System.out.println(modulo);
     }
 }
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+
+
+             //JUST SOME ADDITIONAL STUFF
+    // import java.util.*;
+
+    // class arithmetic_operator {
+    //     public static void main(String[] args) {
+    //         // Scanner sc = new Scanner(System.in);
+    //         // int a = sc.nextInt();
+    //         // System.out.println(a);
+            
+    //         // // int b = sc.nextInt();
+    //         // // System.out.println(b);
+    //         // Scanner sc1 = new Scanner(System.in);
+    //         // int c = sc1.nextInt();
+    //         // System.out.println(c);
+    //         // sc.close();
+            
+    //         // byte a = 10;
+    //         // byte b = 20;
+    //         // byte c = (byte)(a + b);
+    //         // System.out.println(c);
+
+    //         int a = 123456789;
+    //         float f = a;
+    //         System.out.println(f);
+
+
+    //         }
+    // }
