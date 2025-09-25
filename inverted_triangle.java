75f48935991740f1b0a798c6f7c48fb5afe6cb2f
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-09-25 22:36:44 +0530

    inverted triangle star pattern

---
+++ inverted_triangle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class inverted_triangle{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter lines : ");
+        int lines = sc.nextInt();
+        for(int i = 1; i<=lines; i++){
+            for(int j = 1; j<=lines-i+1; j++){
+                System.out.print("*  ");
+            }
+            System.out.println();
+        }
+    }
+}
