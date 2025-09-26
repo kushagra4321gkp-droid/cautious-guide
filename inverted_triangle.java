c0b5ccbc912fa249f6afc94670cc6513fd83ca59
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-09-26 19:17:34 +0530

    modification

--- inverted_triangle.java
+++ inverted_triangle.java
@@ -7,7 +7,7 @@ public class inverted_triangle{
         int lines = sc.nextInt();
         for(int i = 1; i<=lines; i++){
             for(int j = 1; j<=lines-i+1; j++){
-                System.out.print("*  ");
+                System.out.print("*   ");
             }
             System.out.println();
         }
