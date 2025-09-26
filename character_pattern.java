cf781ba2089af3c45ae88f66b72bb4b2609470aa
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-09-26 20:10:29 +0530

    character star pattern

---
+++ character_pattern.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class character_pattern{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter lines : ");
+        int lines = sc.nextInt();
+        char ch = 'A';
+        for(int i = 1; i<=lines; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(ch + "  ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
+    
