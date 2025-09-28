a9e56039700e7aabee116c7ad7f41d81c89890b2
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-09-28 18:32:49 +0530

    call by value

---
+++ callByValue.java
@@ -0,0 +1,12 @@
+public class callByValue{
+
+    public static void changeA(int a){
+        a = 10;
+    }
+
+    public static void main(String args[]){
+        int a = 5;
+        changeA(a);
+        System.out.println(a); //java's function always take copy of variable, not the actual value -> that's why this concept is called "CALL BY VALUE"
+    }
+}
