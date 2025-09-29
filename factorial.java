1dec1e4d4f2012596914a78d499dbab893c08771
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-09-29 09:44:58 +0530

    nCr func

--- factorial.java
+++ factorial.java
@@ -10,10 +10,27 @@ public class factorial{
         return fact;
     }
 
+    public static int calculatenCr(int n, int r){
+        return calculateFact(n) / (calculateFact(r) * calculateFact(n-r));
+    }
+
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        //int num = sc.nextInt();
+        //int factorial = calculateFact(num);
+        //System.out.println("factorial is : " + factorial);
+
+        //Calculating BINOMIAL COEFFICIENT
+        //Formula : C(n, r), is nCr = n! / (r! * (n - r)!)
+
+        System.out.print("enter n : ");
         int n = sc.nextInt();
-        int factorial = calculateFact(n);
-        System.out.println("factorial is : " + factorial);
+        System.out.print("enter r : ");
+        int r = sc.nextInt();
+        long nCr = calculatenCr(n, r);
+        System.out.println("binomial cofficient is : " + nCr);
+
+
+
     }
 }
