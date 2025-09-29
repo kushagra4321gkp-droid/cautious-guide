673af3503d9098ba165bec2427bc28fb21b7db8b
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-09-29 13:05:35 +0530

    overloading functions using dataTypes

--- overloadingUsingDatatypes.java
+++ overloadingUsingDatatypes.java
@@ -8,8 +8,30 @@ public class overloadingUsingDatatypes {
         return a + b;
     }
 
+    public static void checkPrime(int n){
+        boolean isPrime = true;
+        if(n==2){
+            System.out.println("prime");
+        }else{
+            for(int i = 2; i<n; i++){
+            if(n%i==0){
+                isPrime = false;
+                break;
+            }
+            }  
+            if(isPrime == true){
+                System.out.print("prime");
+            }else{
+                System.out.println("not prime");
+            }  
+
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(sum(3, 5));
-        System.out.println(sum(3.2f, 4.8f));
+        //System.out.println(sum(3, 5));
+        //System.out.println(sum(3.2f, 4.8f));
+
+        checkPrime(11);
     }
 }
