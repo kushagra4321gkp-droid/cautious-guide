154ca3721c1ebeeb4b405796ea9bd5644d9994a2
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-10-01 18:04:03 +0530

    License

--- PairsInArray.java
+++ PairsInArray.java
@@ -1,12 +1,15 @@
 public class PairsInArray {
 
     public static void printPairs(int arr[]){
+        int tp = 0;
         for(int i = 0; i<=arr.length-1; i++){
             for(int j = i+1; j<=arr.length-1; j++){
                 System.out.print("(" + arr[i] + "," + arr[j] + ")");
+                tp++;
             }
             System.out.println();
         }
+        System.out.print("Total pairs : " + tp);
     }
 
     public static void main(String[] args) {
