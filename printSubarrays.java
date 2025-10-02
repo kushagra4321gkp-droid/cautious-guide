b32aa4d53d1289f02e77809b1b765477b06b5462
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-10-02 14:07:30 +0530

    max sum in subarrays

--- printSubarrays.java
+++ printSubarrays.java
@@ -1,24 +1,32 @@
+import java.util.*;
+
 public class printSubarrays {
 
     public static void subarrys(int arr[]){
+        int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        
         for(int i = 0; i<=arr.length-1; i++){
-            System.out.println(arr[i]);
-            for(int j = i+1; j<=arr.length-1; j++){
-                //System.out.print("[" + arr[i] + "," + arr[j] + "]");
-                //System.out.print("[");
+            for(int j = i; j<=arr.length-1; j++){
+                sum = 0;
                 for(int k = i; k<=j ; k++){
-                    System.out.print(arr[k] + " ");
+                    sum = sum + arr[k];
                 }
-                //System.out.print("]");
+                System.out.print(sum + " ");
+                if(sum>maxSum){
+                        maxSum = sum;
+                    }
                 System.out.println();
             }
             System.out.println();
-            System.out.println();
+            
         }
+        System.out.println("max sum is : " + maxSum);
     }
 
     public static void main(String[] args) {
-        int[] arr = {2, 4, 6, 8, 10};
+        // int[] arr = {2, 4, 6, 8, 10};
+        int[] arr = {1, -2, 6, -1, 3};
         subarrys(arr);
     }
 }
