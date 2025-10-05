8b3bac738c89a89e723b625df38a679d3bc70438
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-10-05 12:30:36 +0530

    leetcode -> prob 121

---
+++ BuyAndSell_Stocks.java
@@ -0,0 +1,28 @@
+public class BuyAndSell_Stocks {
+
+    public static int maximumProfit(int prices[]){
+        int bp = prices[0];
+        int sp = 0;
+        int profit = 0;
+        int max_profit = 0;
+        for(int i = 1; i<prices.length; i++){
+            sp = prices[i];
+            profit = sp - bp;
+            if(prices[i-1] < bp){
+                bp = prices[i-1];
+            }
+            if(profit > max_profit){
+                max_profit = profit;
+            }
+        }
+        if(max_profit <= 0){
+                return 0;
+            }
+        return max_profit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.print("maximum profit is : " + maximumProfit(prices));
+    }
+}
