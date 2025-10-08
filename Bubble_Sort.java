e07b6498d717f44d630a10a772f7329cbd73bd69
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-10-08 20:52:46 +0530

    somethings modifies in bubble sort that i don't remember

--- Bubble_Sort.java
+++ Bubble_Sort.java
@@ -1,23 +1,29 @@
 public class Bubble_Sort {
 
-    public static void bubbleSort(int arr[]){
+    public static int bubbleSort(int arr[]){
+        int swap = 0;
         for(int i = 0; i<arr.length-1; i++){
             for(int j = 0; j<=arr.length-(2+i); j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swap++;
                 }
+
             }
         }
+        return swap;
     }
 
     public static void main(String[] args) {
         int[] arr = {5, 4, 1, 3, 2};
         //int[] arr = {5, 4, 1, 3, 2, 7, 8, 17, 12, 14}; //testing
-        bubbleSort(arr);
         for(int i = 0; i<arr.length; i++){
             System.out.print(arr[i] + " ");
         }
+        System.out.println();
+
+        System.out.print("number of swaps : " + bubbleSort(arr));
     }
 }
