220ef44c46ad313e18d07414cb4f9ca1a614f2f2
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-10-10 09:08:15 +0530

    Counting sort INCOMPLETE

---
+++ CountingSort.java
@@ -0,0 +1,16 @@
+//  INCOMPLETE
+
+public class CountingSort {
+
+    public static void countingSort(int arr[]){
+        int[] count = new int[arr.length];
+        for(int i = 0; i<arr.length; i++){
+            arr[i]
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 1, 3, 2, 4, 3, 7};
+
+    }
+}
