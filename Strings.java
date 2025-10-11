11c2fd9b7ad9a91a4371a6f4b5f0e867e001ad8a
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-10-11 20:34:23 +0530

    concatination

--- Strings.java
+++ Strings.java
@@ -1,10 +1,27 @@
+import java.util.*;
 public class Strings {
     public static void main(String[] args) {
-        char arr[] = {'a', 'b', 'c', 'd'};
-        String str = "abcd";
-        String str1 = new String("xys");
-        System.out.print(str + " " +  arr + " " +  str1);
+        // char arr[] = {'a', 'b', 'c', 'd'};
+        // String str = "abcd";
+        // String str1 = new String("xys");
+        // System.out.println(str + " " +  arr + " " +  str1);
 
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.print(name);
+
+        // String fullName = "Tony Stark";
+        // System.out.println(fullName.length());
+
+        //concatination
+        String firstName = "Kushagra";
+        String lastName = "Tiwari";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+
+        //To extract a character out of a String, we have a special function
         
+
+
     }
 }
