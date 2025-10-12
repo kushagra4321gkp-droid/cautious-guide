fd27a85afdcca90d8e3d46dd0ad8aa11e96a9071
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-10-12 15:19:48 +0530

    substring(si, ei)

--- String_Comparision.java
+++ String_Comparision.java
@@ -1,8 +1,17 @@
 public class String_Comparision {
+
+    public static String substring(String str, int si, int ei){
+        String string1 = "";
+        for(int i = si; i<ei; i++){
+            string1 += str.charAt(i);
+        }
+        return string1;
+    }
+
     public static void main(String[] args) {
-        String s1 = "Tony";
-        String s2 = "Tony";
-        String s3 = new String("Tony");
+        // String s1 = "Tony";
+        // String s2 = "Tony";
+        // String s3 = new String("Tony");
 
         // if(s1 == s2){
         //     System.out.println("Strings are equal");
@@ -19,10 +28,18 @@ public class String_Comparision {
         //We don't compare two Strings using '==' operator, because it checks string equality on the object level that yes s1 and s1 both are pointing to a same object but for s3 we have created a new object in the Heap Memory!
 
         //correct way to compare two Strings by value
-        if(s1.equals(s3)){
-            System.out.println("s1 and s3 are equal");
-        }else{
-            System.out.println("not equal");
-        }
+        // if(s1.equals(s3)){
+        //     System.out.println("s1 and s3 are equal");
+        // }else{
+        //     System.out.println("not equal");
+        // }
+
+        String str = "Hello World";
+        System.out.println(substring(str, 1, 5));
+
+        System.out.println(str.intern());
+        str.intern();
+
     }
+
 }
