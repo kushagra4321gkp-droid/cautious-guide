92ce0bd32178b742c0f0db90e111d1ba7eff4b99
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-10-13 10:54:56 +0530

    mini program

--- WhyStringBuilder.java
+++ WhyStringBuilder.java
@@ -8,6 +8,22 @@ public class WhyStringBuilder {
             // toString() does not modify the original object.
             // It returns a new String object that contains the same text representation.
 
-        
+        //A mini program using StringBuilder
+        StringBuilder Sb = new StringBuilder("");
+        for(char ch = 'a'; ch<='z'; ch++){
+            Sb.append(ch);
+        }
+        System.out.println();
+        System.out.print(Sb + "\n");
+        System.out.println(Sb.length());
     }
 }
+
+//Concept
+// Analogy
+
+// Think of it like this:
+
+// String → a sealed letter: once written, can’t change; safe to pass around.
+
+// StringBuilder → a notebook: you can write, erase, and change content quickly — but not safe to share until you “seal” it (toString()).
