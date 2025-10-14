c2a8ac23aee8178adca9a7fd47c55ddad2429e45
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-10-14 10:49:24 +0530

    classes and object creation

---
+++ OOPS.java
@@ -0,0 +1,34 @@
+public class OOPS {
+    public static void main(String[] args) {
+        Pen p1 = new Pen();
+        p1.setColor("yellow");
+        System.out.println(p1.color);
+        p1.setTip(5);
+        System.out.println(p1.tip);
+        p1.color = "red";
+        System.out.println(p1.color);
+    }
+}
+
+class Pen {
+    String color;
+    int tip;
+
+    void setTip(int newTip){
+        tip = newTip;
+    }
+
+    void setColor(String newColor){
+        color = newColor;
+    }
+}
+
+class Student {
+    String name;
+    int age;
+    float percentage;
+
+    void calculatePercentage(int phy, int chem, int maths){
+        percentage = (phy + chem + maths) / 3;
+    }
+}
