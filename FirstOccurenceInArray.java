2a793a498180b589ef888604640b4b03788d3941
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-10-20 23:53:18 +0530

    returning index of first occurence of an element in an Array

---
+++ FirstOccurenceInArray.java
@@ -0,0 +1,22 @@
+public class FirstOccurenceInArray {
+    public static void main(String[] args) {
+        int arr[] = {8, 3, 6, 9, 5, 10, 2, 5, 3};
+        int key = 5;
+        int i = 0;
+        System.out.println(firstOccurence(arr, key, i));
+    }
+
+    public static int firstOccurence(int arr[], int key, int i){
+        if(i == arr.length-1){
+            if(arr[i] == key){
+                return i;
+            }
+            return 0;
+        }
+        if(arr[i] == key){
+            return i;
+        }
+        i++;
+        return firstOccurence(arr, key, i);
+    }
+}
