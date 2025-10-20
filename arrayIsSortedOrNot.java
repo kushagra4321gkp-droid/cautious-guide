bdec5f63405b25ae7989416515b1073dfd5d1192
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-10-20 20:22:46 +0530

    check if an array is sorted or not

--- arrayIsSortedOrNot.java
+++ arrayIsSortedOrNot.java
@@ -1,6 +1,21 @@
 public class arrayIsSortedOrNot {
     public static void main(String[] args) {
-        
+        int arr[] = {1, 2, 3, 4, 5};
+        int i = 0;
+        isSorted(arr, 0);
+    }
+
+    public static void isSorted(int arr[], int i){
+        if(i == arr.length-1){
+            System.out.println("sorted");
+            return;
+        }
+        if(arr[i] > arr[i+1]){
+            System.out.println("not sorted!");
+            return;
+        }
+        i++;
+        isSorted(arr, i);
     }
 }
 
