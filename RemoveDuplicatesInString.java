ee0c7f63672781129fb579c207053d80143c36a8
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-10-23 12:39:24 +0530

    remove deplicate characters in a String using StringBuilder and and array to track the status of duplicacy in boolean way

---
+++ RemoveDuplicatesInString.java
@@ -0,0 +1,27 @@
+public class RemoveDuplicatesInString {
+    public static void main(String[] args) {
+        String givenString = "appnnacollege";
+        StringBuilder newStr = new StringBuilder("");
+        // int map[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        boolean map[] = new boolean[26];
+        int i = 0;
+        duplicateRemover(givenString, map, newStr, i);
+        System.out.println(newStr.toString());
+    }
+
+    public static void duplicateRemover(String givenString, boolean map[], StringBuilder newStr, int i){
+        if (i == givenString.length()) {
+            return;
+        }
+
+        char currentChar = givenString.charAt(i);
+        int index = currentChar - 'a';
+
+        if (!map[index]) {
+            newStr.append(currentChar);
+            map[index] = true;
+        }   
+        duplicateRemover(givenString, map, newStr, i+1);
+        
+    }
+}
